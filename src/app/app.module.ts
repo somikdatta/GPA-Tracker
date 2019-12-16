7be1e22bca4f2d1f1ae3fcb31c1f27c525bda065
somikdatta <developersomik@gmail.com>
2019-12-16 20:13:07 +0530

    Beckend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
