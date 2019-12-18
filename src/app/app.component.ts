9d674c4ba1d7b1b38fb45f47f0ae06cf5915d7f3
somikdatta <developersomik@gmail.com>
2019-12-18 14:47:50 +0530

    UI fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    console.log(
+      "Hi Geek,\nI love to be friends with like minded people.\nMessage me, maybe we could help each other.\nCheers,\nSomik Datta"
+    );
+  }
+}
